6249cde5faa8995ba9047b7ace3c8bcb7505eaeb
RonnyVegas2025 <ronnyizidorio46@gmail.com>
2026-01-10 15:40:39 -0300

    Update supabaseClient.js

--- assets/supabaseClient.js
+++ assets/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 // ⚠️ TROQUE PELOS DADOS DO SEU PROJETO SUPABASE
-const SUPABASE_URL = "https://SEU-PROJETO.supabase.co";
-const SUPABASE_ANON_KEY = "SUA_PUBLIC_ANON_KEY_AQUI";
+const SUPABASE_URL = "https://ksyofflresajlpgitqhh.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9....";
 
 export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
